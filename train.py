66c1ccd6bc2a1e6e9e4d29a75c7dab3d0c7f9b6d
candelaria.mosquera <cmosquer@itba.edu.ar>
2021-05-21 19:41:08 -0300

    added wandb fixs

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ def main(args=None):
     config = {
         "batch_size": 2,
         "batch_size_valid":1,
-        "inital_lr": 0.001,
+        "initial_lr": 0.001,
         "lr_scheduler_epochs_interval": 3,
         'lr_scheduler_factor':0.1,
         'dataset': "TX-RX-ds-20210423-00_ubuntu",
