c5f5a758031e4b8af439e066041842db14025d28
candelaria.mosquera <cmosquer@itba.edu.ar>
2021-05-26 20:08:18 -0300

    added test classification support

--- mixed_detection/visualization.py
+++ mixed_detection/visualization.py
@@ -201,7 +201,7 @@ def draw_annotations(image, annotations, color=(0, 255, 0), label_to_name=None,b
     assert('labels' in annotations)
     assert(annotations['boxes'].shape[0] == annotations['labels'].shape[0])
 
-    for i in range(annotations['boxes'].shape[0][0]):
+    for i in range(annotations['boxes'].shape[0]):
         label   = annotations['labels'][i][0][0]
         print('label ',label,'score ',annotations['scores'][i][0][0])
         c       = color if color is not None else label_color(label)
