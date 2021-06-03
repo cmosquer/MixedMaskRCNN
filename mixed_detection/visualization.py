80d7bfcc8386433b5ca20aa6f5925cde27827b2d
candelaria.mosquera <cmosquer@itba.edu.ar>
2021-06-02 21:01:41 -0300

    revised test with save figures

--- mixed_detection/visualization.py
+++ mixed_detection/visualization.py
@@ -206,7 +206,7 @@ def draw_annotations(image, annotations, color=(0, 255, 0), label_to_name=None,b
         label   = annotations['labels'][i]
         try:
             print('LABEL: ',annotations['labels'][i],'SCORE: ', annotations['scores'][i],'BOXES: ',annotations['boxes'][i])
-        except
+        except:
             pass
         c = color if color is not None else label_color(label)
         """ 
