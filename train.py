2a3ffe33842424bb301a7fee3e5e8ec2d58d9683
candelaria.mosquera <cmosquer@itba.edu.ar>
2021-06-15 13:03:23 -0300

    debug

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ def main(args=None):
         'dataset': "TX-RX-ds-20210527-00_ubuntu",
         'revised_test_set' : '{}/{}'.format(experiment_dir,'test_groundtruth_validados.csv'),
         'unfreeze_only_mask': True,
-        'data_augmentation': True,
+        'data_augmentation': False,
         'existing_valid_set': '{}/2021-06-08_boxes_binary/testCSV.csv'.format(experiment_dir), #None
         'opacityies_as_binary':True,
         'no_findings_examples_in_valid': True,
