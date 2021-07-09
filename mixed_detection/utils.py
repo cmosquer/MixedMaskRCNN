1d9e5d6b1c35ef7d6b14251c05209382055ed957
candelaria.mosquera <cmosquer@itba.edu.ar>
2021-07-09 11:37:09 -0300

    test 07-05

--- mixed_detection/utils.py
+++ mixed_detection/utils.py
@@ -438,8 +438,9 @@ def getObjectDetectionHeatmap(boxes,scores,dims,max_alfa=0.2, min_alfa=0):
             #x2 = int(detect[3])
             height = int(y2 - y1)
             width = int(x2 - x1)
-            gradientPadding_x = int(0.2 * width)
-            gradientPadding_y = int(0.2 * height)
+
+            gradientPadding_x = 0#int(width)
+            gradientPadding_y = 0#int(0.2 * height)
 
             try:
                 xlow = max(0,x1-gradientPadding_x)
@@ -499,7 +500,7 @@ def save_heatmap(saving_path,colorimage,outputs):
     plt.close(fig)
     gc.collect()
 
-def gradientCircle(width,height, score, existing_alpha,innerColor=1,outerColor=0.3,max_alfa=0.5,min_alfa=0):
+def gradientCircle(width,height, score, existing_alpha,innerColor=1,outerColor=0.3,max_alfa=0.8,min_alfa=0):
     innerColor = score * innerColor
     outerColor = score * outerColor
     x_arr, y_arr = np.mgrid[0:height, 0:width]
@@ -508,7 +509,7 @@ def gradientCircle(width,height, score, existing_alpha,innerColor=1,outerColor=0
     distanceToCenter = np.sqrt((x_arr - center[0]) ** 2 + (y_arr - center[1]) ** 2) #/ max_dist
     distanceToCenter = (distanceToCenter - distanceToCenter.min()) / (distanceToCenter.max() - distanceToCenter.min())
     r = outerColor * distanceToCenter + innerColor * (distanceToCenter.max() - distanceToCenter)
-    alpha = min_alfa * distanceToCenter + max_alfa * (distanceToCenter.max()  - distanceToCenter)
+    alpha = min_alfa * 0.8*distanceToCenter + max_alfa * (distanceToCenter.max() - 0.8*distanceToCenter)
     alpha = np.where(existing_alpha != 0, 0, alpha)
     assert r.shape == alpha.shape, "Error in shapes {} {}".format(r.shape, alpha.shape)
     circle = np.stack((r, alpha), axis=-1)
