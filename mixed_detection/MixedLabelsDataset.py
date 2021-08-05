11517eda66aa756d89b79b51a7a047df75c31f7e
candelaria.mosquera <cmosquer@itba.edu.ar>
2021-08-05 14:54:06 -0300

    poc test

--- mixed_detection/MixedLabelsDataset.py
+++ mixed_detection/MixedLabelsDataset.py
@@ -81,7 +81,8 @@ class MixedLabelsDataset(torch.utils.data.Dataset):
                     assert os.path.exists(path.replace('\\','/'))
                 except AssertionError:
                     f"{path} does not exist, excluding from dataset"
-                    self.csv = self.csv[~self.csv.file_name==path].reset_index(drop=True)
+                    index = self.csv[self.csv.file_name==path].index
+                    self.csv = self.csv.drop(index,axis=0).reset_index(drop=True)
         self.ids = list(set(self.csv.file_name))
 
     def quantifyClasses(self):
